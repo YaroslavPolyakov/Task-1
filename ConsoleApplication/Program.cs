725bd5c373ed711cbfa61b625e4a6b17fe6e0fcb
Yarolav Polyakov <sensored79@gmail.com>
2018-07-12 11:42:05 +0300

    Some changes

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -25,152 +25,172 @@ namespace ConsoleApplication
                 Title = title;
                 Scription = scription;
             }
-        }
-        public static void ParseCSV()
-        {
-            Console.Write("Path: ");
-            string path = Console.ReadLine();
-            using (var reader = new StreamReader(@path))
+            public static void ParseCSV(string path)
             {
-                List<Data> data = new List<Data>();
-                while (!reader.EndOfStream)
+                try
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
+                    using (var reader = new StreamReader(@path))
+                    {
+                        List<Data> data = new List<Data>();
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(';');
 
-                    data.Add(new Data(values[0], values[1], values[2]));
+                            data.Add(new Data(values[0], values[1], values[2]));
+                        }
+                        Console.WriteLine("Id\tTitle\tScription\t");
+                        foreach (Data item in data)
+                        {
+                            Console.WriteLine("{0}\t{1}\t{2}", item.Id, item.Title, item.Scription);
+                        }
+                    }
                 }
-                Console.WriteLine("Id\tTitle\tScription\t");
-                foreach (Data item in data)
+                catch (Exception e)
                 {
-                    Console.WriteLine("{0}\t{1}\t{2}",item.Id, item.Title, item.Scription);
+                    Console.WriteLine("Error: " + e.Message);
                 }
             }
-        }
-        public static void LoadJson()
-        {
-            string jsonString = "";
-            Console.Write("Path: ");
-            string path = Console.ReadLine();
-            using (StreamReader sr = new StreamReader(@path))
+            public static void ParseJson(string path)
             {
-                while (true)
+                try
                 {
-                    string temp = sr.ReadLine();
-                    if (temp == null) break;
-                    jsonString += "\n" + temp;
+                    string jsonString = "";
+                    using (StreamReader sr = new StreamReader(@path))
+                    {
+                        while (true)
+                        {
+                            string temp = sr.ReadLine();
+                            if (temp == null) break;
+                            jsonString += "\n" + temp;
+                        }
+                    }
+                    Console.WriteLine(jsonString);
+
+                    JArray jsonVal = JArray.Parse(jsonString) as JArray;
+                    dynamic data = jsonVal;
+
+                    foreach (dynamic item in data)
+                    {
+                        Console.WriteLine("{0}\n{1}\n{2}\n{3}", item.firstName, item.lastName, item.age, item.address);
+                        foreach (dynamic cars in item.car)
+                        {
+                            Console.WriteLine("{0}\n{1}\n", cars.model, cars.price);
+                        }
+                    }
                 }
-            }
-            Console.WriteLine(jsonString);
-
-            JArray jsonVal = JArray.Parse(jsonString) as JArray;
-            dynamic data = jsonVal;
-
-            foreach (dynamic item in data)
-            {
-                Console.WriteLine("{0}\n{1}\n{2}\n{3}", item.firstName, item.lastName, item.age, item.address);
-                foreach (dynamic cars in item.car)
+                catch (Exception e)
                 {
-                    Console.WriteLine("{0}\n{1}\n", cars.model, cars.price);
+                    Console.WriteLine("Error: "+e.Message);
                 }
-            }
-
+    }
         }
         public class Monitoring
         {
             //Output all files from subdirectories
-            public static void OutputAllFilse()
+            public static void OutputAllFilse(string path)
             {
-                Console.Write("Path: ");
-                string path = Console.ReadLine();
-                DirectoryInfo dir = new DirectoryInfo(path);
-                foreach (var all in dir.GetFiles("*", SearchOption.AllDirectories))
+                try
                 {
-                    Console.Write("Name: " + all.Name + "\n");
-                    Console.WriteLine("Path: " + all.DirectoryName + "\n");
+                    DirectoryInfo dir = new DirectoryInfo(path);
+                    foreach (var all in dir.GetFiles("*", SearchOption.AllDirectories))
+                    {
+                        Console.Write("Name: " + all.Name + "\n");
+                        Console.WriteLine("Path: " + all.DirectoryName + "\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
                 }
             }
             //Output of a certain format
-            public static void OutputNecessaryFormat()
+            public static void OutputNecessaryFormat(string path, string format)
             {
-                Console.Write("Path: ");
-                string path = Console.ReadLine();
-                DirectoryInfo dir = new DirectoryInfo(path);
-                Console.Write("Input format(example: .txt): ");
-                string format = Console.ReadLine();
-                foreach (var all in dir.GetFiles("*" + format, SearchOption.AllDirectories))
+                try
                 {
-                    Console.Write("Name: " + all.Name + "\n");
-                    Console.WriteLine("Path: " + all.DirectoryName + "\n");
+                    DirectoryInfo dir = new DirectoryInfo(path);
+                    foreach (var all in dir.GetFiles("*" + format, SearchOption.AllDirectories))
+                    {
+                        Console.Write("Name: " + all.Name + "\n");
+                        Console.WriteLine("Path: " + all.DirectoryName + "\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
                 }
             }
             //Read the file
-            public static void ReadFile()
+            public static void ReadFile(string path)
             {
-                string text = "";
-                Console.WriteLine("Input path and name file: ");
-                string file = Console.ReadLine();
-                using (StreamReader fs = new StreamReader(@file))
+                try
                 {
-                    while (true)
+                    string text = "";
+                    using (StreamReader fs = new StreamReader(@path))
                     {
-                        string temp = fs.ReadLine();
-                        if (temp == null) break;
-                        text += "\n" + temp;
+                        while (true)
+                        {
+                            string temp = fs.ReadLine();
+                            if (temp == null) break;
+                            text += "\n" + temp;
+                        }
                     }
+                    Console.WriteLine(text);
                 }
-                Console.WriteLine(text);
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
             }
-
-            public static void Parse()
-            {
-                //
-            }
-
             //Monitoring directories
             [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
-            public static void WatchDirectory()
+            public static void WatchDirectory(List<string> list)
             {
-                //Console.WriteLine("Input path to watch the directory: ");
-                //string path = Console.ReadLine();
-                Console.Write("Input number of directories: ");
-                int number = int.Parse(Console.ReadLine());
-                List<string> list = new List<string>();
-                Console.WriteLine("Input path to watch the directory: ");
-                for (int i = 1; i <= number; i++)
+                try
                 {
-                    list.Add(Console.ReadLine());
+                    foreach (string dir in list)
+                    {
+                        Watch(dir);
+                    }
+                    // Wait for the user to quit the program.
+                    Console.WriteLine("Press \'q\' to quit the sample.");
+                    while (Console.Read() != 'q') ;
                 }
-                foreach (string dir in list)
+                catch (Exception e)
                 {
-                    Watch(dir);
+                    Console.WriteLine("Error: " + e.Message);
                 }
-                // Wait for the user to quit the program.
-                Console.WriteLine("Press \'q\' to quit the sample.");
-                while (Console.Read() != 'q') ;
             }
-            private static void Watch(string watch_folder)
+            public static void Watch(string watch_folder)
             {
-                FileSystemWatcher watcher = new FileSystemWatcher();
-                watcher.Path = watch_folder;
-                // Watch for changes 
-                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-                //Watch all files
-                watcher.Filter = "*";
-                // Add event handlers.
-                watcher.Changed += new FileSystemEventHandler(OnChanged);
-                watcher.Created += new FileSystemEventHandler(OnChanged);
-                watcher.Deleted += new FileSystemEventHandler(OnChanged);
-                watcher.Renamed += new RenamedEventHandler(OnRenamed);
-                // Begin watching.
-                watcher.EnableRaisingEvents = true;
+                try
+                {
+                    FileSystemWatcher watcher = new FileSystemWatcher();
+                    watcher.Path = watch_folder;
+                    // Watch for changes 
+                    watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                    //Watch all files
+                    watcher.Filter = "*";
+                    // Add event handlers.
+                    watcher.Changed += new FileSystemEventHandler(OnChanged);
+                    watcher.Created += new FileSystemEventHandler(OnChanged);
+                    watcher.Deleted += new FileSystemEventHandler(OnChanged);
+                    watcher.Renamed += new RenamedEventHandler(OnRenamed);
+                    // Begin watching.
+                    watcher.EnableRaisingEvents = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
             }
-            private static void OnChanged(object source, FileSystemEventArgs e)
+            public static void OnChanged(object source, FileSystemEventArgs e)
             {
                 // Specify what is done when a file is changed, created, or deleted.
                 Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
             }
-            private static void OnRenamed(object source, RenamedEventArgs e)
+            public static void OnRenamed(object source, RenamedEventArgs e)
             {
                 // Specify what is done when a file is renamed.
                 Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
@@ -178,32 +198,70 @@ namespace ConsoleApplication
         }
         static void Main(string[] args)
         {
+            string path;
             while (true)
             {
-                Console.Write("1.Output all files from subdirectories\n2.Output of a certain format\n3.Read the file\n4.Monitoring directories\n5.Parse json file\n6.Parse csv file\n\n");
+                Console.Write("1.Output all files from subdirectories\n2.Output of a certain format\n3.Read the file\n4.Monitoring directories\n5.Parse json file\n6.Parse csv file\n0.Exit \n\n");
                 string number = Console.ReadLine();
                 switch (number)
                 {
                     case "1":
-                        Monitoring.OutputAllFilse();
-                        break;
+                        {
+                            Console.Write("Path: ");
+                            path = Console.ReadLine();
+                            Monitoring.OutputAllFilse(path);
+                            break;
+                        }
                     case "2":
-                        Monitoring.OutputNecessaryFormat();
-                        break;
+                        {
+                            Console.Write("Path: ");
+                            path = Console.ReadLine();
+                            Console.Write("Input format(example: .txt): ");
+                            string format = Console.ReadLine();
+                            Monitoring.OutputNecessaryFormat(path, format);
+                            break;
+                        }
                     case "3":
-                        Monitoring.ReadFile();
-                        break;
+                        {
+                            Console.WriteLine("Input path and name file: ");
+                            path = Console.ReadLine();
+                            Monitoring.ReadFile(path);
+                            break;
+                        }
                     case "4":
-                        Monitoring.WatchDirectory();
-                        break;
+                        {
+                            Console.Write("Input count of directories: ");
+                            int count = int.Parse(Console.ReadLine());
+                            List<string> list = new List<string>();
+                            Console.WriteLine("Input path to watch the directory: ");
+                            for (int i = 1; i <= count; i++)
+                            {
+                                list.Add(Console.ReadLine());
+                            }
+                            Monitoring.WatchDirectory(list);
+                            break;
+                        }
                     case "5":
-                        LoadJson();
+                        {
+                            Console.Write("Path: ");
+                            path = Console.ReadLine();
+                            Data.ParseJson(path);
+                        }
                         break;
                     case "6":
-                        ParseCSV();
+                        {
+                            Console.Write("Path: ");
+                            path = Console.ReadLine();
+                            Data.ParseCSV(path);
+                        }
+                        break;
+                    case "0":
+                        {
+                            Environment.Exit(0);
+                        }
                         break;
                     default:
-                        Console.WriteLine("Error");
+                        Console.WriteLine(";(");
                         break;
                 }
             }
